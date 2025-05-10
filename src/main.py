f1b4b88833ab9a5e32b72a18393c15307f622c35
Yunus Ibragimov <yunus.ibragimov49@gmail.com>
2025-05-10 16:53:09 +0300

    if сменил на match-case

--- src/main.py
+++ src/main.py
@@ -20,9 +20,13 @@ if __name__ == '__main__':
     for filename in namespace.filenames:
         headers, data = reader.get_staff_data_with_csv(filename)
 
-        if namespace.report == 'payout':
-            report = PayoutReport(headers, data)
+        match namespace.report:
+            case 'payout':
+                report = PayoutReport(headers, data)
 
-            report.generate_report()
+                report.generate_report()
+            
+            case _:
+                raise Exception('Такого вида отчета нет')
 
 # python main.py ../test_data/data3.csv ../test_data/data1.csv ../test_data/data2.csv -r payout
